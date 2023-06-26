107962f8dc023831e60cafc75ccef7d3ac35135e
Иван <jazzman80@yandex.ru>
2023-06-27 00:41:35 +0300

    Редизайн карточки постов

--- app/src/main/java/com/skillbox/humblr/main/posts/PostItem.kt
+++ app/src/main/java/com/skillbox/humblr/main/posts/PostItem.kt
@@ -1,11 +1,10 @@
 package com.skillbox.humblr.main.posts
 
 import android.content.res.Configuration
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -17,55 +16,69 @@ import androidx.constraintlayout.compose.Dimension
 import com.skillbox.humblr.entity.Post
 import com.skillbox.humblr.entity.PostData
 import com.skillbox.humblr.theme.AppTheme
+import com.skillbox.humblr.theme.bodySmall
 import com.skillbox.humblr.theme.labelLarge
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PostItem(
     item: Post,
     onClick: () -> Unit
 ) {
-    Card(
-        colors = CardDefaults.cardColors(
-            containerColor = MaterialTheme.colorScheme.surface
-        ),
-        onClick = { onClick() }
-    ) {
-        ConstraintLayout(
-            modifier = Modifier.fillMaxSize()
-        ) {
-            val (title, subscribeButton) = createRefs()
-
-            Text(
-                text = item.data.title,
-                modifier = Modifier
-                    .constrainAs(title) {
-                        top.linkTo(parent.top)
-                        start.linkTo(parent.start)
-                        end.linkTo(subscribeButton.start)
-                        bottom.linkTo(parent.bottom)
-                        width = Dimension.fillToConstraints
-                    }
-                    .padding(top = 12.dp, start = 12.dp, bottom = 12.dp),
-                style = labelLarge
+    ConstraintLayout(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .background(
+                color = MaterialTheme.colorScheme.surface,
+                shape = MaterialTheme.shapes.medium
             )
 
-//            SubscribeButton(
-//                modifier = Modifier
-//                    .constrainAs(subscribeButton) {
-//                        top.linkTo(parent.top)
-//                        end.linkTo(parent.end)
-//                        bottom.linkTo(parent.bottom)
-//                    }
-//                    .padding(top = 12.dp, end = 12.dp, bottom = 12.dp),
-//                onSubscribe = { isSubscribed ->
-//                    onSubscribe(isSubscribed)
-//                },
-//                initSubscribe = item.data.userIsSubscriber
-//            )
+    ) {
+        val (title, subscribeButton, author) = createRefs()
+        val startGuide = createGuidelineFromStart(12.dp)
+        val endGuide = createGuidelineFromEnd(12.dp)
 
-        }
+        Text(
+            text = item.data.title,
+            modifier = Modifier
+                .constrainAs(title) {
+                    top.linkTo(parent.top, margin = 12.dp)
+                    start.linkTo(startGuide)
+                    end.linkTo(endGuide)
+                    width = Dimension.fillToConstraints
+                },
+            style = labelLarge,
+            color = MaterialTheme.colorScheme.onSurface
+        )
+
+        //            SubscribeButton(
+        //                modifier = Modifier
+        //                    .constrainAs(subscribeButton) {
+        //                        top.linkTo(parent.top)
+        //                        end.linkTo(parent.end)
+        //                        bottom.linkTo(parent.bottom)
+        //                    }
+        //                    .padding(top = 12.dp, end = 12.dp, bottom = 12.dp),
+        //                onSubscribe = { isSubscribed ->
+        //                    onSubscribe(isSubscribed)
+        //                },
+        //                initSubscribe = item.data.userIsSubscriber
+        //            )
+
+        Text(
+            text = "author",
+            style = bodySmall,
+            color = MaterialTheme.colorScheme.primary,
+            modifier = Modifier
+                .constrainAs(author) {
+                    top.linkTo(title.bottom, margin = 20.dp)
+                    start.linkTo(startGuide)
+                }
+                .padding(bottom = 12.dp)
+        )
+
     }
+
 }
 
 @Preview(
